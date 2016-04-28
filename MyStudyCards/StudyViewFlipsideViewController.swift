48dddb32fc23d3ec885a5d8269cbb5a4546f1972
Noah Brusky <bruskynd@msoemac06simac.nebula.msoe.edu>
2016-04-28 12:43:47 -0500

    added flip side of card

---
+++ MyStudyCards/StudyViewFlipsideViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudyViewFlipsideViewController.swift
+//  MyStudyCards
+//
+//  Created by Noah Brusky on 4/28/16.
+//  Copyright © 2016 MSOE SE-4910i. All rights reserved.
+//
+
+import UIKit
+
+class StudyViewFlipsideViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
