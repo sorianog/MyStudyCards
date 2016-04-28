0e47a7cd1ee7c189789485defa84409cfa901594
Gerald Soriano <sorianog@msoe.edu>
2016-04-28 11:34:52 -0500

    Made changes to MyDecks VC.

--- MyStudyCards/MyDecksViewController.swift
+++ MyStudyCards/MyDecksViewController.swift
@@ -33,3 +33,23 @@ class MyDecksViewController: ViewController {
     */
 
 }
+
+extension MyDecksViewController: UITableViewDataSource {
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cellIdentifier = "CurrDecksCell"
+    var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+    if cell == nil {
+      cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+    }
+    cell.textLabel!.text = "My Deck"
+    return cell
+  }
+}
+
+extension MyDecksViewController: UITableViewDelegate {
+  
+}
