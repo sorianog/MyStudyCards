e449ca0f92718fd954c6e43fee26b50c6c667bca
Gerald Soriano <sorianog@msoe.edu>
2016-05-05 11:41:04 -0500

    Added logic to update the "Correct" and "Incorrect" amount labels of the Results screen based on actions in the Study Session screen.

--- MyStudyCards/StudyViewViewController.swift
+++ MyStudyCards/StudyViewViewController.swift
@@ -8,43 +8,49 @@
 
 import UIKit
 
+var correct = 0
+var incorrect = 0
+
 class StudyViewViewController: ViewController {
 
-    @IBAction func thumbsUpClicked() {
-        //for now, this just advances to the results screen
-        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-        
-        let nextViewController = storyBoard.instantiateViewControllerWithIdentifier("resultsViewController") as UIViewController
-        self.presentViewController(nextViewController, animated:true, completion:nil)
-    }
+
+  @IBAction func thumbsUpClicked() {
+    correct++
+    //for now, this just advances to the results screen
+    let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let nextViewController = storyBoard.instantiateViewControllerWithIdentifier("resultsViewController") as UIViewController
+    self.presentViewController(nextViewController, animated:true, completion:nil)
+  
+  }
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
 
-        let value = UIInterfaceOrientation.LandscapeLeft.rawValue
-        UIDevice.currentDevice().setValue(value, forKey: "orientation")
-        
-        // Do any additional setup after loading the view.
-    }
+      let value = UIInterfaceOrientation.LandscapeLeft.rawValue
+      UIDevice.currentDevice().setValue(value, forKey: "orientation")
+      
+      // Do any additional setup after loading the view.
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-    
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+  override func shouldAutorotate() -> Bool {
+      return true
+  }
+  
 
-    /*
-    // MARK: - Navigation
+  /*
+  // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
+  }
+  */
 
 }
